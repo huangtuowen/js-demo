ed328b0b87381ad0635f0776d9ce40ceb49d56d3
huangtuowen <huangtuowen@gmail.com>
2018-07-04 18:10:46 +0800

    webpack-dev-server

--- src/hello.js
+++ src/hello.js
@@ -3,4 +3,4 @@ import {
 } from './common.js';
 import './hello.css';
 
-setHtml('app1', '<p class="hello">hello webpack!<p>');
+setHtml('app', '<p class="hello">hello webpack-dev-server!<p>');
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
     mode: "development",
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     entry: {
         hello: './src/hello.js'
     },
