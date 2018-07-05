60110672b0b70f605c7ce96b2548e7f965ef5cbb
huangtuowen <huangtuowen@gmail.com>
2018-07-05 11:30:01 +0800

    uglify

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,6 +1,10 @@
 const merge = require('webpack-merge');
 const config = require('./webpack.base.conf.js');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(config, {
-  mode: "production"
+  mode: "production",
+  plugins: [
+    new UglifyJsPlugin()
+  ]
 });
